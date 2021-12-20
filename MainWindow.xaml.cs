2f633507b667c750ffef14b99a014a4a329a54e6
Andreiko_0 <92972300+MeDemneLiss@users.noreply.github.com>
2021-12-20 05:04:44 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Data;
+namespace PracticalWork_9
+{
+    public partial class MainWindow : Window
+    {
+        DataRow row = res.NewRow();
+        Questionnaire firstResponden = new Questionnaire("Смирнова Виктория Иосифовна", "ж", 1967, "г.Ленинград", "русская");
+        Questionnaire secondResponden = new Questionnaire("Шельмах Адам Васильевич", "м", 1993 ,"г. Ульяновск" ,"немец");
+        Questionnaire thirdResponden = new Questionnaire("Мичурина Нина Арсеньевна", "ж", 1986, "г. Есентуки", "русская");
+        Questionnaire fourthResponden = new Questionnaire("ДюженковаПелагея Тимофеевна", "ж",1995, "г. Севастополь", "украинка");
+        Questionnaire fifthResponden = new Questionnaire("Боброва Лидия Юлиановна", "ж",1981, "г. Казань", "татарка");
+        public static DataTable res = new DataTable();
+        private Questionnaire respondent;
+
+        private void FillQuestionnaire_Click(object sender, RoutedEventArgs e)
+        {
+            res.Columns.Add("ФИО");
+            res.Columns.Add("пол");
+            res.Columns.Add("Год рождения");
+            res.Columns.Add("Место рождения");
+            res.Columns.Add("Национальность");
+            questionnaire.ItemsSource = res.DefaultView;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 0) respondent = firstResponden; 
+                if (i == 1)  respondent = secondResponden; 
+                if (i == 2)  respondent = thirdResponden; 
+                if (i == 3) respondent = fourthResponden; 
+                if (i == 4) respondent = fifthResponden;
+                row[0] = respondent.FullName;
+                row[1] = respondent.Gender;
+                row[2] = respondent.YearBirth;
+                row[3] = respondent.Birthplace;
+                row[4] = respondent.Nationality;
+                res.Rows.Add(row);
+                row = res.NewRow();
+            }
+            questionnaire.ItemsSource = res.DefaultView;
+        }
+
+        private void AverageAge_Click(object sender, RoutedEventArgs e)
+        {
+            int allAge = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 0) respondent = firstResponden;
+                if (i == 1) respondent = secondResponden;
+                if (i == 2) respondent = thirdResponden;
+                if (i == 3) respondent = fourthResponden;
+                if (i == 4) respondent = fifthResponden;
+                allAge =+ Convert.ToInt32(respondent.YearBirth)  ;
+            }
+            averageAgeOut.Text = Convert.ToString(allAge/5); 
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Самсаков Андрей Александрович ИСП-31\nПрактическая работа №8\n Создать интерфейс - человек. Создать классы – студент и студент-отец семейства. Классы должны включать конструкторы, функцию для формирования строки информации о студенте. Сравнение производить по фамилии. ", "Информация о программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Questionnaire.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticalWork_9
+{
+    struct Questionnaire
+    {
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public int YearBirth { get; set; }
+        public string Nationality { get; set; }
+        public string Birthplace { get; set; }
+
+        public Questionnaire(string fullName, string gender, int yearBirth, string nationality, string birthplace)
+        {
+            FullName = fullName;
+            Gender = gender;
+            YearBirth = yearBirth;
+            Birthplace = birthplace;
+            Nationality = nationality;
+        }
+    }
+}
